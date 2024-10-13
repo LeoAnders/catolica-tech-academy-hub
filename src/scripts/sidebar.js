054cfb1d59160197fd7915e4afdc366db91dd046
LeoAnders <leonardoanders15@gmail.com>
2024-10-13 00:30:00 -0300

    Correção da naveção entre conteúdos na sidebar

--- src/scripts/sidebar.js
+++ src/scripts/sidebar.js
@@ -1,21 +1,30 @@
+let isSidebarClick = false; // Variável para saber se a navegação foi feita pela sidebar
+
 document.querySelectorAll(".nav-link").forEach((item) => {
   item.addEventListener("click", function () {
+    isSidebarClick = true;
+    console.log(isSidebarClick);
     document
       .querySelectorAll(".nav-link")
       .forEach((link) => link.classList.remove("active"));
     this.classList.add("active");
+
+    setTimeout(() => {
+      isSidebarClick = false;
+    }, 500);
   });
 });
 
 window.addEventListener("message", function (event) {
   const page = event.data;
   const links = document.querySelectorAll(".nav-link");
-
-  links.forEach((link) => {
-    link.classList.remove("active");
-
-    if (link.getAttribute("href").includes(page)) {
-      link.classList.add("active");
-    }
-  });
+  console.log(isSidebarClick);
+  if (!isSidebarClick) {
+    links.forEach((link) => {
+      link.classList.remove("active");
+      if (link.getAttribute("href").includes(page)) {
+        link.classList.add("active");
+      }
+    });
+  }
 });
